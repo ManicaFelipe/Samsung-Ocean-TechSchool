79b4694ef242314392691cf3a49f5a275eaed4ae
Felipe Manica <felipe.manicaeng@gmail.com>
2023-08-31 01:44:42 +0000

    Criando rotas usando Flask no Python

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask("meu app") 
+
+@app.route('/')
+def hello():
+    return "Hello, World!"
+
+
+@app.route('/novo')
+def novo():
+    return "<h1>Nova Página</h1>" 
